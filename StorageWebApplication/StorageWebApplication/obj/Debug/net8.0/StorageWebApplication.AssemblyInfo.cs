51d8c71cce4d609a1c41844cb851b1f98a9ceddc
CsMargareta <csmargareta06@gmail.com>
2024-12-19 07:14:26 +0100

    hibák javítása

--- StorageWebApplication/StorageWebApplication/obj/Debug/net8.0/StorageWebApplication.AssemblyInfo.cs
+++ StorageWebApplication/StorageWebApplication/obj/Debug/net8.0/StorageWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StorageWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a705c84c1689f1b46805927a6f60d0351a9879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26285be986e3a8b8d36258d61ecb3f5a930af573")]
 [assembly: System.Reflection.AssemblyProductAttribute("StorageWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StorageWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
